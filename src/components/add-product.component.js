534b60aa0d741ead91c1555b3c594b6120d180a6
thetradecoder <thetradecoder@gmail.com>
2020-10-14 12:43:39 +0600

    local update

--- src/components/add-product.component.js
+++ src/components/add-product.component.js
@@ -8,14 +8,19 @@ export default function AddProduct(){
     const [productCategory, setProductCategory] = useState("");
     const [productSize, setProductSize] = useState("");
 
+    function onSubmitEnlistNewProduct(e){
+        e.preventDefault();
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={onSubmitEnlistNewProduct}>
                 <h2>Add a Product Name</h2>
                 <input type="text" placeholder="product id"/>
                 <input type="text" placeholder="product name"/>
                 <input type="text" placeholder="product category"/>
                 <input type="text" placeholder="product size"/>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
     );
