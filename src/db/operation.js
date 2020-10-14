fcf7a69008480a4343a397ded4addc605e68c7f1
thetradecoder <thetradecoder@gmail.com>
2020-10-14 13:33:23 +0600

    local update

---
+++ src/db/operation.js
@@ -0,0 +1,23 @@
+export default function Action(action){
+   this.action = action;
+
+
+   createTable(){
+       const sql = `CREATE TABLE IF NOT EXIST TradeCoderApp(
+           ID INTEGER PRIMARY KEY,
+           PRODUCT_NAME TEXT,
+           PRODUCT_CAT TEXT,
+           PRODUCT_SIZE TEXT,
+           BUY_COST INTEGER,
+           SALE_PRICE INTEGER,
+           BUY_DATE TEXT,
+           SALE_DATE TEXT,
+           STOCK INTEGER,
+           TOTAL_BUY_QTY INTEGER,
+           TOTAL_SALE_QTY INTEGER,
+           TOTAL_BUY INTEGER,
+           TOTAL_SALES INTEGER,
+           STMT_DATE TEXT,
+       )`
+   }
+}
