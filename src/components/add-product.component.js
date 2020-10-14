9422db498b1a5a17fa0cd8e26b5a41c45ad7d425
thetradecoder <thetradecoder@gmail.com>
2020-10-14 12:31:12 +0600

    local update

--- src/components/add-product.component.js
+++ src/components/add-product.component.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function AddProduct(){
@@ -8,8 +9,11 @@ export default function AddProduct(){
     return(
         <div>
             <form>
+                <h2>Add a Product Name</h2>
                 <input type="text" placeholder="product id"/>
                 <input type="text" placeholder="product name"/>
+                <input type="text" placeholder="product category"/>
+                <input type="text" placeholder="product size"/>
             </form>
         </div>
     );
