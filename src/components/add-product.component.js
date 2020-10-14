960cecbbf9fd17685a21bfcc2fc7478f468d8a4d
thetradecoder <thetradecoder@gmail.com>
2020-10-14 12:57:39 +0600

    local update

--- src/components/add-product.component.js
+++ src/components/add-product.component.js
@@ -8,6 +8,19 @@ export default function AddProduct(){
     const [productCategory, setProductCategory] = useState("");
     const [productSize, setProductSize] = useState("");
 
+    function onChangeProductName(e){
+        setProductName(e.target.value);
+    }
+    function onChangeProductId(e){
+        setProductId(e.target.value);
+    }
+    function onChangeProductCategory(e){
+        setProductCategory(e.target.value);
+    }
+    function onChangeProductSize(e){
+        setProductSize(e.target.value);
+    }
+
     function onSubmitEnlistNewProduct(e){
         e.preventDefault();
     }
@@ -16,10 +29,10 @@ export default function AddProduct(){
         <div>
             <form onSubmit={onSubmitEnlistNewProduct}>
                 <h2>Add a Product Name</h2>
-                <input type="text" placeholder="product id"/>
-                <input type="text" placeholder="product name"/>
-                <input type="text" placeholder="product category"/>
-                <input type="text" placeholder="product size"/>
+                <input type="text" placeholder="product id" onChange={onChangeProductName}/>
+                <input type="text" placeholder="product name" onChange={onChangeProductId}/>
+                <input type="text" placeholder="product category" onChange={onChangeProductCategory}/>
+                <input type="text" placeholder="product size" onChange={onChangeProductSize}/>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
