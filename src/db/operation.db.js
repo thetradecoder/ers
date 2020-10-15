00947590dc93ea7c4672e192a08778190ff1aed2
thetradecoder <thetradecoder@gmail.com>
2020-10-15 15:38:13 +0600

    local update

--- src/db/operation.db.js
+++ src/db/operation.db.js
@@ -1,2 +1,28 @@
 const sqlite3 = window.require('sqlite3');
+const Promise = window.require('bluebird');
 
+
+class DbAccess{
+    constructor(db){
+        this.db = new sqlite3.Database(db);
+    }
+
+    run(sql, params=[]){
+        return new Promise((resolve, reject)=>{
+            this.db.run(sql, params, (err)=>{
+                err?reject():resolve();
+            })
+        })
+
+    }
+
+    get(sql, params=[]){
+        return new Promise((resolve, reject)=>{
+            this.db.get(sql, params, (err)=>{
+                err?reject():resolve();
+            })
+        })
+    }
+
+
+}
